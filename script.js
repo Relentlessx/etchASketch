467260747f3aa09fac0804635965b39ba7d99ce3
DavidG <david.guzelj8@gmail.com>
2022-10-09 09:51:05 +0200

    Necessary files added

---
+++ script.js
@@ -0,0 +1,7 @@
+for(x=0; x<256; x++){
+    let divBoard = document.createElement('div')
+    divBoard.className = "colorDivs";
+
+    let boardWrapper = document.getElementById('divWrapper');
+    boardWrapper.appendChild(divBoard);
+}
