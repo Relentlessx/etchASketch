436352a10fc06d6eeb5fa097d46f0a2f6b7c778d
DavidG <david.guzelj8@gmail.com>
2022-10-09 10:28:38 +0200

    working grid creator

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-for(x=0; x<256; x++){
-    let divBoard = document.createElement('div')
-    divBoard.className = "colorDivs";
+const container = document.getElementById('divWrapper');
+let divRow = document.getElementsByClassName('gridRow');
+let divCell = document.getElementsByClassName('gridCell');
 
-    let boardWrapper = document.getElementById('divWrapper');
-    boardWrapper.appendChild(divBoard);
+function createGrid(rowNum, cellNum){
+    makeRows(rowNum);
+    makeCollumns(cellNum);
 }
+
+function makeRows(rowNum){
+    for(x = 0; x < rowNum; x++){
+        let row = document.createElement('div');
+        container.appendChild(row).className = "gridRow";
+    }
+}
+
+function makeCollumns(cellNum){
+    for (i = 0; i < divRow.length; i++){
+        for (j = 0; j < cellNum; j++){
+            let newCell = document.createElement("div");
+            divRow[j].appendChild(newCell).className = "gridCell"
+        }
+    }
+}
+
+createGrid(16, 16)
